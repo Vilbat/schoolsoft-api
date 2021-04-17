36fa59758019087349c10672f5d6a10dcbb95aa7
Vilmer Åström <vilmer.astrom@gmail.com>
2021-04-17 10:56:36 +0200

    changed variable name

--- example.py
+++ example.py
@@ -7,9 +7,9 @@ schoolsoft = Schoolsoft('username', 'password')
 assignments = schoolsoft.get_sorted_assignments()
 newest_assignment = schoolsoft.get_newest_assignment()
 
-for i in assignments:
-    print(i.name)
-    print(i.description)
-    print(i.end_date)
-    print(i.assignment_type)
+for j in assignments:
+    print(j.name)
+    print(j.description)
+    print(j.end_date)
+    print(j.assignment_type)
     print('\n')
